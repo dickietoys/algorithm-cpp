e21abd5df9c0cb28dce2a1630330d44fb5e889e5
deetch <deetch>
2019-10-28 22:45:41 +0800

    init

--- src/dp/LongestCommonSubSequence.h
+++ src/dp/LongestCommonSubSequence.h
@@ -74,9 +74,52 @@ class LongestCommonSubSequence : public TestBase
 
   int DpFindAux(const string &s1, const string &s2)
   {
-    vector<vector<int>> bookmark()
+    vector<vector<int>> bookmark(s1.size(), vector<int>(s2.size(), 0));
+    if (s1[0] == s2[0])
+    {
+      bookmark[0][0] = 1;
+    }
 
-    return 0;
+    for (size_t i = 1; i < s2.size(); ++i)
+    {
+      if (s1[0] == s2[i])
+      {
+        bookmark[0][i] = 1;
+      }
+      else
+      {
+        bookmark[0][i] = bookmark[0][i - 1];
+      }
+    }
+
+    for (size_t i = 1; i < s1.size(); ++i)
+    {
+      if (s1[i] == s2[0])
+      {
+        bookmark[i][0] = 1;
+      }
+      else
+      {
+        bookmark[i][0] = bookmark[i - 1][0];
+      }
+    }
+
+    for (size_t i = 1; i < s1.size(); ++i)
+    {
+      for (size_t j = 1; j < s2.size(); ++j)
+      {
+        if (s1[i] == s2[j])
+        {
+          bookmark[i][j] = bookmark[i-1][j-1] + 1;
+        }
+        else
+        {
+          bookmark[i][j] = std::max(bookmark[i-1][j], bookmark[i][j-1]);
+        }
+      }
+    }
+
+    return bookmark[s1.size() - 1][s2.size() - 1];
   }
 };
 
