b0efecf295d4f13975364f17b67e63cdc143646f
deetch <deetch>
2019-11-04 23:36:04 +0800

    init

--- src/dp/FindLongestPath.h
+++ src/dp/FindLongestPath.h
@@ -37,7 +37,7 @@ class FindLongestPath : public TestBase
     {
       for (size_t j = 0; j < matrix[i].size(); ++j)
       {
-        max = std::max(BruteFindAux(matrix, i, j, 0) + 1, max);
+        max = std::max(BruteFindAux(matrix, i, j, matrix[i][j] - 1), max);
       }
     }
 
@@ -46,7 +46,22 @@ class FindLongestPath : public TestBase
 
   int DpFind(vector<vector<int>> &matrix)
   {
-    return DpFindAux(matrix);
+    vector<vector<int>> bookmark(matrix.size(), vector<int>(matrix[1].size(), -1));
+    int max = 0;
+
+    for (size_t i = 0; i < matrix.size(); ++i)
+    {
+      for (size_t j = 0; j < matrix[i].size(); ++j)
+      {
+        if (bookmark[i][j] == -1)
+        {
+          DpFindAux(matrix, i, j, matrix[i][j] - 1, bookmark);
+        }
+        max = std::max(bookmark[i][j], max);
+      }
+    }
+
+    return max;
   }
 
  private:
@@ -75,9 +90,39 @@ class FindLongestPath : public TestBase
     return std::max({size1, size2, size3, size4});
   }
 
-  int DpFindAux(vector<vector<int>> &matrix)
+  int DpFindAux(vector<vector<int>> &matrix,
+                int xpos,
+                int ypos,
+                int prevValue,
+                vector<vector<int>> &bookmark)
   {
-    return 0;
+    if (xpos < 0 || xpos >= static_cast<int>(matrix.size()))
+    {
+      return 0;
+    }
+
+    if (ypos < 0 || ypos >= static_cast<int>(matrix[xpos].size()))
+    {
+      return 0;
+    }
+
+    if (bookmark[xpos][ypos] != -1)
+    {
+      return bookmark[xpos][ypos];
+    }
+
+    int curValue = matrix[xpos][ypos];
+    if (curValue - prevValue != 1)
+    {
+      return 0;
+    }
+    int size1 = 1 + BruteFindAux(matrix, xpos + 1, ypos, curValue);
+    int size2 = 1 + BruteFindAux(matrix, xpos - 1, ypos, curValue);
+    int size3 = 1 + BruteFindAux(matrix, xpos, ypos + 1, curValue);
+    int size4 = 1 + BruteFindAux(matrix, xpos, ypos - 1, curValue);
+
+    bookmark[xpos][ypos] = std::max({size1, size2, size3, size4});
+    return bookmark[xpos][ypos];
   }
 
   void showBookmark(vector<vector<int>> &bookmark)
