fcfe16b32db9b41c4bb7e03fe2c3e8bc4996066b
deetch <deetch>
2019-11-24 23:47:58 +0800

    init

--- src/tmp/main.cc
+++ src/tmp/main.cc
@@ -13,12 +13,32 @@ public:
     vector<int> arr = {1, 5, 8, 9, 10, 17, 17, 20};
     int result = BruteResult(arr);
     cout << "BruteResult: " << result << endl;
+    result = DpResult(arr);
+    cout << "DpResult: " << result << endl;
+
+    arr = {3, 5, 8, 9, 10, 17, 17, 20};
+    result = BruteResult(arr);
+    cout << "BruteResult: " << result << endl;
+    result = DpResult(arr);
+    cout << "DpResult: " << result << endl;
     cout << "=========================CutRod=========================" << endl;
   }
 
   int DpResult(vector<int> &arr)
   {
+    int rodSize = arr.size();
+    vector<int> bookmark(rodSize + 1, 0);
+    bookmark[1] = arr[0];
+    for (int i = 2; i <= rodSize; ++i)
+    {
+      for (int j = 0; j < i; ++j)
+      {
+        int remain = i - j;
+        bookmark[i] = std::max(bookmark[j] + arr[i-j-1], bookmark[i]);
+      }
+    }
 
+    return bookmark[rodSize];
   }
 
   int BruteResult(vector<int> &arr)
