2dd3105913ef3d35963ea0d1720ea9ae730a0838
seventh <seventh>
2020-03-25 20:46:38 +0800

    init

---
+++ src/leetcode/172.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_set>
+#include <cassert>
+#include <stack>
+#include <limits>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+ public:
+  void RunTest()
+  {
+  }
+
+  int trailingZeroes(int n) {
+    if (n < 5)
+    {
+      return 0;
+    }
+
+    return n / 5 + trailingZeroes(n / 5);
+  }
+
+  template<class T>
+  void Show(vector<T> &result)
+  {
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+      cout << result[i] << ", ";
+    }
+    cout << endl;
+  }
+
+  template<class T>
+  void Show(vector<vector<T>> &result)
+  {
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+      for (size_t j = 0; j < result[i].size(); ++j)
+      {
+        cout << result[i][j] << ", ";
+      }
+      cout << endl;
+    }
+  }
+};
+
+int main()
+{
+  Solution *solution = new Solution();
+  solution->RunTest();
+
+  return 0;
+}
---
+++ src/leetcode/173.cc
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <unordered_set>
+#include <cassert>
+#include <stack>
+#include <limits>
+#include <deque>
+#include <unordered_set>
+#include <unordered_map>
+#include <sstream>
+#include <iterator>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class BSTIterator {
+  stack<TreeNode *> st;
+ public:
+  BSTIterator(TreeNode* root) {
+    push_node(root);
+  }
+
+  void push_node(TreeNode *node)
+  {
+    while (node)
+    {
+      st.push(node);
+      node = node->left;
+    }
+  }
+
+  /** @return the next smallest number */
+  int next() {
+    TreeNode *node= st.top();
+    st.pop();
+    if (node->right)
+    {
+      push_node(node->right);
+    }
+    return node->val;
+  }
+
+  /** @return whether we have a next smallest number */
+  bool hasNext() {
+    return !st.empty();
+  }
+};
+
+class Solution {
+ public:
+  void RunTest()
+  {
+  }
+
+  template<class T>
+  void Show(vector<T> &result)
+  {
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+      cout << result[i] << ", ";
+    }
+    cout << endl;
+  }
+
+  template<class T>
+  void Show(vector<vector<T>> &result)
+  {
+    for (size_t i = 0; i < result.size(); ++i)
+    {
+      for (size_t j = 0; j < result[i].size(); ++j)
+      {
+        cout << result[i][j] << ", ";
+      }
+      cout << endl;
+    }
+  }
+};
+
+int main()
+{
+  Solution *solution = new Solution();
+  solution->RunTest();
+
+  return 0;
+}
