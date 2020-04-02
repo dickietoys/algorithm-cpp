4dd124c906f06578505860ad0d3d7ce9d7898af5
deetch <deetch>
2020-04-02 23:00:15 +0800

    fixed

--- src/leetcode/188.cc
+++ src/leetcode/188.cc
@@ -28,8 +28,40 @@ class Solution {
   {
   }
 
-  int maxProfit(int k, vector<int>& prices) {
+  int maxProfit(int m, vector<int>& prices) {
+    // dp[k, i] = max(dp[k, i-1], prices[i] - prices[j] + dp[k-1, j-1]), j=[0..i-1]
+    int prices_size = prices.size();
+    if (prices_size <= 1)
+    {
+      return 0;
+    }
 
+    if (m >= prices_size / 2)
+    {
+      int max_value = 0;
+      for (int i = 1; i < prices_size; ++i)
+      {
+        if (prices[i] > prices[i-1])
+        {
+          max_value += prices[i] - prices[i-1];
+        }
+      }
+      return max_value;
+    }
+
+    int max_transaction = m;
+    vector<vector<int>> dp(max_transaction + 1, vector<int>(prices_size, 0));
+    for (int k = 1; k <= max_transaction; ++k)
+    {
+      int min = prices[0];
+      for (int i = 1; i < prices_size; ++i)
+      {
+        min = std::min(min,  prices[i] - dp[k-1][i-1]);
+        dp[k][i] = std::max(dp[k][i-1], prices[i] - min);
+      }
+    }
+
+    return dp[max_transaction][prices_size-1];
   }
 
   template<class T>
