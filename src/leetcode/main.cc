5863049dca74d6e47c67a861f61324fef3e686d9
seventh <seventh>
2020-09-04 17:36:28 +0800

    init

--- src/leetcode/main.cc
+++ src/leetcode/main.cc
@@ -37,63 +37,37 @@ class Solution {
  public:
   void RunTest()
   {
-    double result = findMedianSortedArrays({1,3}, {2});
-    cout << "{1,3}, {2}; result: " << result << endl;
-    result = findMedianSortedArrays({1,2}, {3,4});
-    cout << "{1,2}, {3,4}; result: " << result << endl;
-    result = findMedianSortedArrays({0,0}, {0,0});
-    cout << "{0,0}, {0,0}; result: " << result << endl;
-    result = findMedianSortedArrays({}, {1});
-    cout << "{}, {1}; result: " << result << endl;
-    result = findMedianSortedArrays({2}, {});
-    cout << "{2}, {}; result: " << result << endl;
-
   }
 
   /*
-    [1 2]
-    [3 4]
+    matrix = [
+      [ 1,  5,  9],
+      [10, 11, 13],
+      [12, 13, 15]
+    ],
 
-    [1] [2]
-    [3] [4]
+    [1 5 9 19 11 12 13 13 15]
+    mid = 8,
+    count = 7
 
-    [1][2] []
-    []     [3 4]
-
-    [1] [2,3]
-
-    [2 3 4] []
+    return 13.
   */
-  double findMedianSortedArrays(vector<int> nums1, vector<int> nums2) {
-    if (nums1.size() > nums2.size())
+  int kthSmallest(vector<vector<int>>& matrix, int k) {
+    int left = matrix.front().front();
+    int right = matrix.back().back();
+    while (left <= right)
     {
-      std::swap(nums1, nums2);
+      int mid = left + (right - left) / 2;
+      int count = 0;
+      for (int i = 0; i < matrix.size(); ++i)
+      {
+        count += std::upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+      }
+
+      // 比k大的的
+
+      if (count )
     }
-
-    int left1_size = 0;
-    int left2_size = 0;
-    int total_size = nums1.size() + nums2.size();
-    int left_pos = 0;
-    int right_pos = nums1.size();
-    while (left_pos <= right_pos)
-    {
-      left1_size = left_pos + (right_pos - left_pos) / 2;
-      left2_size = (total_size + 1) / 2 - left1_size;
-      if (left1_size > 0 && left2_size < nums2.size() && nums1[left1_size - 1] > nums2[left2_size])
-      {
-        right_pos = left1_size - 1;
-      }
-      else if (left2_size > 0 && left1_size < nums1.size() && nums2[left2_size - 1] > nums1[left1_size])
-      {
-        left_pos = left1_size + 1;
-      }
-      else
-      {
-
-      }
-    }
-
-    return 0;
   }
 
   template<class T>
